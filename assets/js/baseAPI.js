4482f513ba0617dbd7ca9d66a447f1de56dbc7ed
guoyibo <2597362878@qq.com>
2022-05-04 22:10:28 +0800

    试试

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,13 +3,13 @@ $.ajaxPrefilter(function(options) {
   options.url = 'http://api-breakingnews-web.itheima.net' + options.url
   console.log(options.url);
 
-  options.complete = function(res) {
+  // options.complete = function(res) {
 
-    //   console.log('执行了complete回调');
-    //   console.log(res);
-    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
-      localStorage.removeItem('token')
-      location.href = '/login.html'
-    }
-  }
+  //   //   console.log('执行了complete回调');
+  //   //   console.log(res);
+  //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+  //     localStorage.removeItem('token')
+  //     location.href = '/login.html'
+  //   }
+  // }
 })

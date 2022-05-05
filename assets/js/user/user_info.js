332534984c8c5797ae90371e531181aba1677003
guoyibo <2597362878@qq.com>
2022-05-05 21:18:08 +0800

    密码已经实现

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -23,4 +23,23 @@ $(function() {
       }
     })
   }
+  $('#btnReset').on('click', function(e) {
+    e.preventDefault();
+    initUserInfo();
+  })
+  $('.layui-form').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/my/userinfo',
+      data: $(this).serialize(),
+      success: function(res) {
+        if (res.status !== 0) {
+          return layer.msg('更新用户信息失败')
+        }
+        layer.msg('更新用户信息成功')
+        window.parent.getUserInfo()
+      }
+    })
+  })
 })
